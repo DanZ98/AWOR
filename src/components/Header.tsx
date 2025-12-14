4c7a318553d8815cd02ccf568388f60f66d1c43c
Daniel Zins <danzins15@gmail.com>
2025-12-14 16:37:37 -0600

    Modified global and header styles and added About AWOR button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,24 @@ import '../styles/header-styles/header-styles.scss';
 
 const Header = () => {
   return (
-    <section className='header'>
-      <div className='header-section'>
-        <h1 className='header-title'>Ace Way of Retailing</h1>
-        <p className='header-content'>
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est
-          laborum.
-        </p>
-      </div>
-    </section>
+		<section className="header">
+			<div className="header-section">
+				<h1 className="header-title">Ace Way of Retailing</h1>
+				<p className="header-content">
+					Your Destination for Ace's Best Practices on All Key Aspects
+					of Your Operations
+				</p>
+				<p className='header-aworbtn-container'>
+					<a
+						href="https://acenet.aceservices.com/awor"
+						className="about-awor-btn"
+            target="_blank"
+					>
+						About AWOR
+					</a>
+				</p>
+			</div>
+		</section>
   );
 };
 
