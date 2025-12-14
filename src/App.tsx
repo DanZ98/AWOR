abe890834af44745e9a4314924e819061e17ff88
Daniel Zins <danzins15@gmail.com>
2025-12-13 22:33:15 -0600

    Removed header, footer and hamburger components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import HeaderMain from "./components/HeaderMain";
 import Header from "./components/Header";
 import IconSelection from "./components/IconSelection";
 import StoreOpportunities from "./components/StoreOpportunities";
 import Topics from "./components/Topics";
 import Contact from "./components/Contact";
-import FooterMain from "./components/FooterMain";
 
 const App = () => {
   return (
     <div id="awor-wrapper">
-      <HeaderMain />
       <Header />
       <IconSelection />
       <StoreOpportunities />
       <Topics />
       <Contact />
-      <FooterMain />
     </div>
   );
 };
